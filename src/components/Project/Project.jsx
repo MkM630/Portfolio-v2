f9aa234ac12a156bc21ace42f85f516c924f7437
MkM630 <mainakmaitra33@gmail.com>
2025-02-11 17:23:04 +0530

    v-2.0

--- src/components/Project/Project.jsx
+++ src/components/Project/Project.jsx
@@ -1,23 +1,29 @@
 // ProjectSection.js
 import React from 'react';
 import './Project.css';
+import image1 from '../../assets/image1.png';
+import image2 from '../../assets/image2.png';
+import image3 from '../../assets/image3.png';
 
 const Project = () => {
   const projects = [
     {
-      title: 'Project 1',
-      description: 'Description of Project 1',
-      imageUrl: 'https://via.placeholder.com/150',
+      title: 'Trusto_E_learning_Page',
+      description: 'Used: HTML, CSS, JavaScript',
+      imageUrl: image1,
+      githubLink: 'https://github.com/MkM630/Trusto_E_learning_Page'
     },
     {
-      title: 'Project 2',
-      description: 'Description of Project 2',
-      imageUrl: 'https://via.placeholder.com/150',
+      title: 'Find Mole',
+      description: 'Used: HTML, CSS, JavaScript,Css animation',
+      imageUrl: image2,
+      githubLink: 'https://github.com/MkM630/Find_the_mole'
     },
     {
-      title: 'Project 3',
-      description: 'Description of Project 3',
-      imageUrl: 'https://via.placeholder.com/150',
+      title: 'Liquid stock market',
+      description: 'Used: HTML, CSS,Tailwind, JavaScript',
+      imageUrl: image3,
+      githubLink: 'https://github.com/MkM630/Liquid_StockMarket_AnalyzePage'
     },
   ];
 
@@ -30,11 +36,19 @@ const Project = () => {
             <img src={project.imageUrl} alt={project.title} />
             <h3>{project.title}</h3>
             <p>{project.description}</p>
+            <a
+              href={project.githubLink}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="github-button"
+            >
+              View on GitHub
+            </a>
           </div>
         ))}
       </div>
       <a
-        href="https://github.com/your-username"
+        href="https://github.com/MkM630"
         target="_blank"
         rel="noopener noreferrer"
         className="see-more-button"
